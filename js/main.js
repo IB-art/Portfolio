f2203add253ec72a4b0c484a6130f786074d63b1
IB-art <65600199+IB-art@users.noreply.github.com>
2021-09-07 11:56:32 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(function(){
-  $('.certificados__box').magnificPopup({
+$(function () {
+	$('.certificados__box').magnificPopup({
 		delegate: 'a',
 		type: 'image',
 		tLoading: 'Loading image #%curr%...',
@@ -7,12 +7,32 @@ $(function(){
 		gallery: {
 			enabled: true,
 			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
 		},
 	});
 
-	$('.menu__btn').on('click', function(){
-    $('.menu__list').toggleClass('menu__list--active');
-  });
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
+	});
+
+
+	const smothScrollElems = document.querySelectorAll('a[href^="#"]:not(a[href="#"])');
+
+	smothScrollElems.forEach(link => {
+		link.addEventListener('click', (event) => {
+
+			event.preventDefault()
+			const id = link.getAttribute('href').substring(1)
+
+			document.getElementById(id).scrollIntoView({
+				behavior: 'smooth'
+			});
+
+
+		})
+	})
+
+
+
 
 });
